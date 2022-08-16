eb20fa54b93ff9f940ce1ee3f3c1cf76e3c1f066
Philip Lewis <philipjonlewis@gmail.com>
2022-08-16 21:40:57 +0800

    added comments

--- src/routes/publicRoutes.ts
+++ src/routes/publicRoutes.ts
@@ -8,10 +8,32 @@ import {
 
 import { publicRouteQuerySanitizer } from "../middleware/sanitization/publicRouteSanitizer";
 
+/**
+ * * Router : Public Route
+ * * Description : Route to get all users and all products
+ * * Done as per test instructions
+ * ! User Activities :
+ * ! - Get All Users
+ *    - optional count & skip query parameters
+ * ! - Get All Products
+ *    - optional count & skip query parameters
+ */
+
+// Both routes will have the query params sanitizer to prevent parameter pollution
 router.use(publicRouteQuerySanitizer);
 
-router.route("/users").get([getAllUsersController]);
+router.route("/users").get([
+  // ! Route : http://localhost:4000/api_v1/public/users?count=0&skip=0
+  // * Description : API Endpoint for getting all users
+  // ! Optional query parameter for productId to get just one product
+  getAllUsersController,
+]);
 
-router.route("/products").get([getAllProductsController]);
+router.route("/products").get([
+  // ! Route : http://localhost:4000/api_v1/public/products?count=0&skip=0
+  // * Description : API Endpoint for getting all products
+  // ! Optional query parameter for productId to get just one product
+  getAllProductsController,
+]);
 
 export default router;
